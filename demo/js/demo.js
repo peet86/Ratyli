024aeecbca8700fd53757665eae1d8ce59f4d72e
Peter Varga <varga.peter@binvision.hu>
2015-11-24 23:01:56 -0800

    demo stuff

---
+++ demo/js/demo.js
@@ -0,0 +1,57 @@
+/* GitHub: https://github.com/peet86/Ratyli */
+
+$(function() {
+	// Default
+	$("#demo1 .ratyli").ratyli();
+
+	// Configure with Datasets
+	$("#demo2 .ratyli").ratyli();
+
+	// Configure with JS
+	$("#demo3 .ratyli").ratyli({rate:3,max:7});
+
+	// Custom Signs
+	$("#demo4 .ratyli").ratyli();
+
+	// Font Awesome Signs
+	$("#demo5 .ratyli").ratyli({
+		full:"<i class='fa fa-thumbs-up'></i>",
+		empty:"<i class='fa fa-thumbs-o-up'></i>",
+
+	});
+
+	// Rated Callback
+	$("#demo6 .ratyli").ratyli({
+		onRated:function(value,init){
+			// rating callback
+			if(!init) alert(value);  // prevent run at init
+		},
+	});
+
+	// Sign Callbacks:
+	$("#demo7 .ratyli").ratyli({
+		onSignClick:function(target){
+			// sign click event callback
+			alert("clicked: "+target);
+		},
+		onSignEnter:function(target){
+			// sign mouseenter event callback
+			console.log("enter");
+		},
+		onSignLeave:function(target){
+			// sign mouseleave event callback
+			console.log("leave");
+		},
+	});
+
+	// Custom cursor
+	$("#demo8 .ratyli").ratyli({cursor:"crosshair"});
+
+	// Disabled
+	$("#demo9 .ratyli").ratyli({disable:true});
+
+	// Unrateable
+	$("#demo10 .ratyli").ratyli({unrateable:true});
+
+
+});
