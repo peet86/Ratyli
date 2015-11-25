15a832d8264cd0702526240a6719804100756992
Peter Varga <varga.peter@binvision.hu>
2015-11-24 23:32:39 -0800

    callback parameter fix,  unrateable feature, rate and ratemax data attribute fix

--- jquery.ratyli.js
+++ jquery.ratyli.js
@@ -28,6 +28,7 @@
                 
                 base.options.onSignClick.call(base,target);
                 var val=$(target).prevAll().length+1;
+
                 base.set(val);
               }
             });
@@ -40,7 +41,7 @@
                   $(target).addClass("rate-active");
                   $(target).prevAll().addClass("rate-active");
                 }
-                base.options.onSignEnter.call(base,target);
+                base.options.onSignEnter.apply(null,[base.options.rate,target]);
             });
 
             base.$el.on("mouseleave","> *",function(e){
@@ -51,22 +52,33 @@
                   $(target).removeClass("rate-active");
                   $(target).prevAll().removeClass("rate-active");
                 }
-                base.options.onSignLeave.call(base,target);
+                base.options.onSignLeave.apply(null,[base.options.rate,target]);
             });
 
         };
       
         base.set=function(val,init){
-            if(val<0 || (val % 1 != 0) || val>base.options.max) val=0; // reset to 0.
-          
-            base.options.rate=val;          
+            if(val<0 || (val % 1 != 0) || val>base.options.ratemax) val=0; // reset to 0.
+
+            if(val==1 && base.options.rate==1 && base.options.unrateable==true && !init){
+                val=0;
+            }
+
+            base.options.rate=val;
  
             // reset html
             base.$el.html("");
-          
-             // generate signs
+
+            // set data-rate
+            if (base.options.rate!=0) base.$el.attr("data-rate",base.options.rate);
+
+            // set data-ratemax
+            base.$el.attr("data-ratemax",base.options.ratemax);
+
+
+            // generate signs
              var i=0;
-             while (i < base.options.max) {
+             while (i < base.options.ratemax) {
                   var tmp="";
                   if(i<base.options.rate){
                     tmp=base.signTemplate("full");
@@ -75,11 +87,14 @@
                   }
                   base.$el.append(tmp);
                   i++;
-            } 
+            }
             
             // set rated
-            if(!init && !base.options.disable) base.$el.addClass("rated");
-            
+            if(!init && !base.options.disable){
+                base.$el.addClass("rated");
+                base.$el.attr("data-rate",val);
+            }
+
             //rated callack
             base.options.onRated.call(base,val,init);
           
@@ -96,12 +111,13 @@
     };
     
     $.ratyli.defaultOptions = {
-        disable:false,
+        disable: false,
+        unrateable: false,
         full: "★",
         empty: "☆",
         cursor:"default",
         rate:0,
-        max:5,
+        ratemax:5,
         onSignEnter:function(){},
         onSignLeave:function(){},
         onSignClick:function(){},
